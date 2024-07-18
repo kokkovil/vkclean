e6c5ff275fbd94a7af7dbe7e2ea1893d0edf07be
Ville Kokkonen <p14659@edu.sasky.fi>
2024-07-18 15:14:45 +0300

    config-määrityksille oma tiedosto

--- public/index.php
+++ public/index.php
@@ -1,23 +1,25 @@
 <?php
 
-  // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
-  // Oletetaan, että sovellus sijaitsee polussa /~vkokkone/vkclean.
-  $request = str_replace('/~vkokkone/vkclean', '', $_SERVER['REQUEST_URI']);
-  $request = strtok($request, '?');
+require_once '../src/init.php';
 
-  // Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava käsittelijä.
-  if ($request === '/' || $request === '/etusivu') {
-    echo '<h1>Tervetuloa VKCleanin kotisivuille</h1>';
-  } else if ($request === '/palvelut') {
-    echo '<h1>Palvelumme</h1>';
-  } else if ($request === '/yhteystiedot') {
-    echo '<h1>Yhteystiedot</h1>';
-  } else if ($request === '/hinnasto') {
-    echo '<h1>Hinnasto</h1>';
-  } else if ($request === '/tietoa-yrityksesta') {
-    echo '<h1>Tietoa yrityksestä</h1>';
-  } else {
-    echo '<h1>Pyydettyä sivua ei löytynyt</h1>';
-  }
+$config = require_once '../config/config.php';
+
+$request = str_replace($config['urls']['baseUrl'], '', $_SERVER['REQUEST_URI']);
+
+// Selvitetään mitä sivua on kutsuttu ja suoritetaan sivua vastaava käsittelijä.
+if ($request === '/' || $request === '/etusivu') {
+  echo '<h1>Tervetuloa VKCleanin kotisivuille</h1>';
+} else if ($request === '/palvelut') {
+  echo '<h1>Palvelumme</h1>';
+} else if ($request === '/yhteystiedot') {
+  echo '<h1>Yhteystiedot</h1>';
+} else if ($request === '/hinnasto') {
+  echo '<h1>Hinnasto</h1>';
+} else if ($request === '/tietoa-yrityksesta') {
+  echo '<h1>Tietoa yrityksestä</h1>';
+} else {
+  echo '<h1>Pyydettyä sivua ei löytynyt</h1>';
+}
 
 ?>
+
