1870a4796f214b2ea116f7f309f80bbf2ed5a86f
Ville Kokkonen <p14659@edu.sasky.fi>
2024-07-21 09:32:38 +0300

    tietokantakäsittelyn kääreluokka

--- config/config.php
+++ config/config.php
@@ -1,10 +1,17 @@
 <?php
 
 $config = array(
+    "db" => array(
+       "dbname" => $_SERVER["DB_DATABASE"],
+       "username" => $_SERVER["DB_USERNAME"],
+       "password" => $_SERVER["DB_PASSWORD"],
+       "host" => "localhost"
+    ),
     "urls" => array(
         "baseUrl" => "/~vkokkone/vkclean"
     )
-);
+  );
+
 define("PROJECT_ROOT", dirname(__DIR__) . "/");
 define("HELPERS_DIR", PROJECT_ROOT . "src/helpers/");
 define("TEMPLATE_DIR", PROJECT_ROOT . "src/view/");
