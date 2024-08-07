d80d20bd0523224c1808821302844ff025e88030
Ville Kokkonen <p14659@edu.sasky.fi>
2024-08-07 15:02:43 +0300

    lisää css

--- src/view/palvelut.php
+++ src/view/palvelut.php
@@ -1,22 +1,22 @@
 <?php $this->layout('template', ['title' => 'Palvelut']) ?>
 
-<h1>Palvelumme</h1>
+<h1 style="text-align: center; font-size: 2.5rem;">Palvelumme</h1>
 
 <div class='palvelut'>
 <?php foreach ($palvelut as $palvelu_item): ?>
     <div class="palvelu-item">
         <!-- Testataan JavaScriptiä Linkki, joka kutsuu JavaScript-funktiota -->
         <div class="palvelu-link">
-            <a href='javascript:void(0)' onclick='toggleService(<?= $palvelu_item["palvelu_id"] ?>)'>
+            <a href='javascript:void(0)' onclick='toggleService(<?= $palvelu_item["palvelu_id"] ?>)' style="font-size: 1.5rem;">
                 <?= htmlspecialchars($palvelu_item['palvelu_nimi']) ?>
             </a>
         </div>
         
         <!-- Palvelun sisältö, joka näytetään/piilotetaan klikkauksen mukaan -->
         <div id="palvelu-content-<?= $palvelu_item['palvelu_id'] ?>" class="palvelu-content" style="display: <?= ($palvelu && $palvelu_item['palvelu_id'] == $palvelu['palvelu_id']) ? 'block' : 'none' ?>;">
-            <h2 style="font-size: 2rem;"><?= htmlspecialchars($palvelu_item['palvelu_nimi']) ?></h2>
-            <div><?= htmlspecialchars($palvelu_item['palvelu_kuvaus']) ?></div>
-            <div>Hinta: <?= htmlspecialchars($palvelu_item['palvelu_hinta']) ?> €</div>
+            <h2 style="font-size: 1rem;"><?= htmlspecialchars($palvelu_item['palvelu_nimi']) ?></h2>
+            <div style="font-size: 1rem;"><?= htmlspecialchars($palvelu_item['palvelu_kuvaus']) ?></div>
+            <div style="font-size: 1rem;">Hinta: <?= htmlspecialchars($palvelu_item['palvelu_hinta']) ?> €</div>
         </div>
     </div>
 <?php endforeach; ?>
@@ -32,6 +32,7 @@ function toggleService(id) {
     }
 }
 </script>
+
 
 
 
